1f506c15df4ae3ae555f791d8cd9b6fda9ce7fb0
eddardyang <eddardyang@tencent.com>
2022-11-29 16:40:25 +0800

    first commit

---
+++ src/components/virtual-scroll-list/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+        <div className='list'></div>
+            </div>
+    );
+}
+
+export default App;
